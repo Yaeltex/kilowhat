75f6c59c9d8897e27f81461c39808e36c355441a
sanotronics <sanno89@gmail.com>
2017-03-08 14:11:59 -0300

    Fixed config file loading

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ MODE_LABELS = (_("Off"), _("Note"), _("CC"), _("NRPN"),_("Program Change"), _("S
 MODE_ENABLED = [MODE_OFF, MODE_NOTE, MODE_CC, MODE_NRPN, MODE_PC, MODE_SHIFTER]
 
 config = configparser.ConfigParser()
-configFilePath = r’ioconfig.txt'
+configFilePath = r'ioconfig.txt'
 config.readfp(open(configFilePath))
 
 MAX_INPUTS_CC = int(config.get('IO Config', 'maxInputsCC'))
